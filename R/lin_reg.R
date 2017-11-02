b4e66888b69b1300ca995b28c6d7b1d10d2aae4e
liam <liam.k.loscalzo@gmail.com>
2017-11-02 12:41:31 -0400

    more work in class

--- R/lin_reg.R
+++ R/lin_reg.R
@@ -32,3 +32,38 @@ linmodEst <- function(x, y) {
     df = df
   )
 }
+
+linmod <- function(x, ...)  UseMethod("linmod")
+
+linmod.default <- function(x, y, ...) {
+  x <- as.matrix(x)
+  y <- as.numeric(y)
+  est <- linmodEst(x, y)
+  est$fitted.values <- as.vector(x %*% est$coefficients)
+  est$residuals <- y - est$fitted.values
+  est$call <- match.call()
+  class(est) <- "linmod"
+  return(est)
+}
+
+print.linmod <- function(x, ...) {
+  cat("Call:\n")
+  print(x$call)
+  cat("\nCoefficients:\n")
+  print(x$coefficients)
+}
+
+predict.linmod <- function(x, ...) {
+  print("I don't know how to do that")
+}
+
+linmod.formula <- function(formula, data = list(), ...) {
+  mf <- model.frame(formula = formula, data = data)
+  x <- model.matrix(attr(mf, "terms"), data = mf)
+  y <- model.response(mf)
+  est <- linmod.default(x, y, ...)
+  est$call <- match.call()
+  est$formula <- formula
+  return(est)
+}
+
---
+++ tests/testthat/test_coef.R
@@ -0,0 +1,7 @@
+data(cats, package = "MASS")
+l1 <- linmod(Hwt ~ Bwt * Sex, data = cats)
+l2 <- lm(Hwt ~ Bwt * Sex, data = cats)
+
+test_that("same estimated coefficients as lm function", {
+  expect_equal(round(l1$coefficients, 3), round(l2$coefficients, 3))
+})
